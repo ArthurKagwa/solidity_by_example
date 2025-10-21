2950f5742a9cab220996dd7132f162e0caa1866c
Arthur Asasira <asasiraarthur@gmail.com>
2025-10-22 01:05:37 +0300

    Primitive datatypes

---
+++ DataTypes.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract DataTypes{
+    // bool
+    bool public boolean = true;
+    bool public unsetBoolean;
+    // uint
+    uint8 public u8=3;
+    uint256 public u256=3000;
+    uint public u=3;
+    // int 
+    int public a=-1
+    int256 public b= 2000;
+    // bytes
+    bytes public x= 0xff;
+
+}
