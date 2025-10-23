4218bb26a5c21086f6398058b049c6dc5914df23
Arthur Asasira <asasiraarthur@gmail.com>
2025-10-23 14:29:43 +0300

    fix: file name

--- ImmutablePerson.sol
+++ ImmutablePerson.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifire: MIT
 
 pragma solidity ^0.8.26;
-contract immutablePerson {
+contract ImmutablePerson {
     string public immutable name;
     constructor(string _name) {
         name = _name;
     }
+    ImmutablePerson public arthur = ImmutablePerson("arthur");
 }
 
 // values of immutable variables cannot be changed after object instactiation through the constructor.
